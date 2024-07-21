a449480c7453f131c91a57095b236f507cab77e1
hank <hankmo@gmail.com>
2024-07-21 23:31:35 +0800

    test: add test codes

--- 01-basic/02-data-type/dict.py
+++ 01-basic/02-data-type/dict.py
@@ -3,6 +3,7 @@
 1、字典是无序的，通过hash计算内存存储位置
 2、字典的key必须是不可变序列，即不能进行增删改，如字符串，可变的如列表
 """
+
 # 相当于JSON中的对象，Java中的Map
 
 # 创建空字典
@@ -12,16 +13,16 @@ print(dict1, id(dict1), type(dict1))
 print(dict2, id(dict2), type(dict2))
 
 # 创建字典: key: value的格式
-dict1 = {"name": '张三', "age": 10, "from": 'china'}
+dict1 = {"name": "张三", "age": 10, "from": "china"}
 print(dict1)
 
 # 获取元素的值
-print(dict1['name'])  # 如果key不存在，会抛出异常
+print(dict1["name"])  # 如果key不存在，会抛出异常
 print(dict1.get("name"))  # 如果key不存在，返回none
 print(dict1.get("name1"))  # 如果key不存在，返回None,第二个参数为指定的值
 print(dict1.get("name1", "unknown-user"))  # unknown-user
 # 删除元素，使用pop方法
-dict1.pop("name") # {'age': 10, 'from': 'china'}
+dict1.pop("name")  # {'age': 10, 'from': 'china'}
 print(dict1)
 
 # 遍历字典
